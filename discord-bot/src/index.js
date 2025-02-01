fc1ac62214de5e31bbaa0312f5c499be0d31c69b
Leandro Bataglia <leandro.bataglia@gmail.com>
2025-02-01 16:33:50 -0300

    [feat] adiciona comando !contribution <gh-user> ao bot

--- discord-bot/src/index.js
+++ discord-bot/src/index.js
@@ -1,5 +1,6 @@
 import { Client, IntentsBitField } from "discord.js";
 import { config } from "dotenv";
+import { generateImage, getContributions } from "./github-graph.js";
 
 config()
 
@@ -22,4 +23,26 @@ client.on('messageCreate', (message) => {
     }
 })
 
+client.on('messageCreate', async (message) => {
+    if (message.content.startsWith('!contributions')) {
+      const username = message.content.split(' ')[1];
+      try {
+        const weeks = await getContributions(username);
+        const imageBuffer = generateImage(weeks, 2025); // Gera o gráfico para 2025
+  
+        // Envia a imagem como um arquivo anexo
+        await message.channel.send({
+          files: [{
+            attachment: imageBuffer,
+            name: 'contributions_2025.png' // Nome do arquivo
+          }]
+        });
+      } catch (error) {
+        console.error(error);
+        message.channel.send(`Erro ao obter contribuições: ${error.message}`);
+      }
+    }
+  });
+
 client.login(process.env.TOKEN)
+
